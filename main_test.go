e71b5b4448153d15c59c88f6ade8c7851af22388
Chris Smith <csmith@platformconsulting.io>
2021-10-05 12:52:07 -0400

    Expand tests

--- main_test.go
+++ main_test.go
@@ -4,26 +4,48 @@ import "testing"
 
 //fizzbuzz should return fizz if evenly divisible by 3
 func TestFizzBuzz_returnsFizz(t *testing.T) {
-	s := fizzBuzz(3)
-	if s != "fizz" {
-		t.Fail()
+	numbers := []int64{3, 6, 9, 12, 18, 21, 24, 27, 33, 36, 39}
+	for _, num := range numbers {
+		s := fizzBuzz(num)
+		if s != "fizz" {
+			t.Fail()
+		}
 	}
 
 }
 
 //fizzbuzz should return buzz if evenly divisible by 5
 func TestFizzBuzz_returnsBuzz(t *testing.T) {
-	s := fizzBuzz(5)
-	if s != "buzz" {
-		t.Fail()
+	numbers := []int64{5, 10, 20, 25, 35, 40, 50, 55, 65}
+	for _, num := range numbers {
+		s := fizzBuzz(num)
+		if s != "buzz" {
+			t.Fail()
+		}
 	}
+
 }
 
 //fizzBuzz should return FizzBuzz if evenly divisible by both
 func TestFizzBuzz_returnsFizzBuzz(t *testing.T) {
-	s := fizzBuzz(15)
-	if s != "fizzbuzz" {
-		t.Fail()
+	numbers := []int64{15, 30, 45, 60, 75, 90, 105, 120, 135}
+	for _, num := range numbers {
+		s := fizzBuzz(num)
+		if s != "fizzbuzz" {
+			t.Fail()
+		}
+	}
+}
+
+//fizzbuzz should return an empty string when a number is neither evenly divisible
+//by 3 or 5
+func TestFizzBuzz_returnsEmptyString(t *testing.T) {
+	numbers := []int64{1, 2, 4, 7, 8, 101, 202, 304}
+	for _, num := range numbers {
+		s := fizzBuzz(num)
+		if s != "" {
+			t.Fail()
+		}
 	}
 
 }
